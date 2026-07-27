d792e7f58d4e052e38c08dba855b785f2e972ae2
kawaimasa <kawai@noce-creative.co.jp>
2026-07-27 15:03:53 +0900

    Configure Cloudflare D1 database ID

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "4e398c40-2dc9-4abd-964e-0882276409ea";
 
 const { d1, r2 } = hostingConfig;
 
